b04fd20e6279dce79e1dd43e30b99438571fc8b2
João Fernando Rangel <joaofrguimaraes1@gmail.com>
2023-09-27 07:44:06 -0300

    Update RNG.py

--- RNG_DATA_FOR_MQTT/RNG.py
+++ RNG_DATA_FOR_MQTT/RNG.py
@@ -6,23 +6,23 @@
 #Dados ambientes (a serem medidos)
 #UM%%numero%%TEMP%%numero%%Radiação%%numero%%Inclinação%%X%%Y%%Z
 import random
-import Classes
+import Classes as C
 from datetime import datetime
 import time
 
 #insira os limites para as variáveis
 #Os limites servem para todas as variáveis da classe, portanto os valores são compartilhados por tensão e corrente.
-Classes.muda_limites(Classes.Modulo_fixo_1, 5,73)
-Classes.muda_limites(Classes.Modulo_fixo_2, 0,100)
-Classes.muda_limites(Classes.Modulo_fixo_3, 0,100)
-Classes.muda_limites(Classes.Modulo_fixo_4, 0,100)
-Classes.muda_limites(Classes.Modulo_movel_1, 0,100)
-Classes.muda_limites(Classes.Modulo_movel_2, 0,100)
+C.muda_limites(C.Modulo_fixo_1, 5,73)
+C.muda_limites(C.Modulo_fixo_2, 0,100)
+C.muda_limites(C.Modulo_fixo_3, 0,100)
+C.muda_limites(C.Modulo_fixo_4, 0,100)
+C.muda_limites(C.Modulo_movel_1, 0,100)
+C.muda_limites(C.Modulo_movel_2, 0,100)
 
 arquivo = open("valores.txt", "w+")
 
-#random.seed(Classes.muda_seed())
-Classes.Modulo_fixo_1.tensao = random.randrange(Classes.Modulo_fixo_1.lim_sup )
-arquivo.write("tensao = " + str(Classes.Modulo_fixo_1.tensao) + "\n")
-Classes.Modulo_fixo_1.corrente = random.randrange(Classes.Modulo_fixo_1.lim_sup )
-arquivo.write("corrente = " + str(Classes.Modulo_fixo_1.corrente))
+#random.seed(C.muda_seed())
+C.Modulo_fixo_1.tensao = random.randrange(C.Modulo_fixo_1.lim_sup )
+arquivo.write("tensao = " + str(C.Modulo_fixo_1.tensao) + "\n")
+C.Modulo_fixo_1.corrente = random.randrange(C.Modulo_fixo_1.lim_sup )
+arquivo.write("corrente = " + str(C.Modulo_fixo_1.corrente))
